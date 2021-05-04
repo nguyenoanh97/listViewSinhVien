c0d3cb44831594f8e79e7b9e6c0fe3a10cb42c2f
oanh0444@gmail.com <anhnguyetsol97@gmail.com>
2021-05-04 17:34:01 +0700

    Oanh: custom adapter

---
+++ app/src/main/java/com/example/listviews/SinhVien.java
@@ -0,0 +1,11 @@
+package com.example.listviews;
+
+public class SinhVien {
+    public String HoTen;
+    public Integer NamSinh;
+
+    public SinhVien(String hoTen, Integer namSinh) {
+        HoTen = hoTen;
+        NamSinh = namSinh;
+    }
+}
